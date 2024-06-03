831e3cefe3f53171ee79285b2c6ac49b7f52e07a
Nu-nuu <unnu2569@gmail.com>
2024-06-03 20:14:56 +0700

    content

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home_social">
+            <a href="https://www.facebook.com/unnu2569" className="home_social-icon" target='_blank'>
+                <i className="uil uil-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/Nuu_ltt" className="home_social-icon" target='_blank'>
+                <i className="uil uil-instagram"></i>
+            </a>
+            <a href="https://github.com/Nu-nuu" className="home_social-icon" target='_blank'>
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
